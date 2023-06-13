5282ae61b36857bd0ffc274d8020322691baab37
Manarbek.2501 <b.manarbek@icloud.com>
2023-06-13 23:49:02 +0600

    Initial Commit

---
+++ SneakerStore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SneakerStore
+//
+//  Created by Manarbek Bibit on 13.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
